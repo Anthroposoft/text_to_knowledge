8888a435e421079f5490575114e7f6a695babea5
Anthroposoft <140537644+Anthroposoft@users.noreply.github.com>
2024-02-04 21:41:12 +0100

    Better file names

--- examples/project_gutenberg/run.sh
+++ examples/project_gutenberg/run.sh
@@ -1,15 +1,15 @@
 # 1. Transform all ebooks into JSON format, so we can add summaries, question, answers and categories
 # books_to_json --input_list=./ebook_texts/00_file_list.txt --input_dir=./ebook_texts --output_dir=./json
 # 2. Add two different summaries, a simpler and a reflective one to chunks, but only a single form to chapters
-augment_books add_chunk_summaries --input_dir=json/ --config=configs/add_detailed_chunk_summaries_config.py
-augment_books add_chunk_summaries --input_dir=json/ --config=configs/add_detailed_chunk_summaries_reflect_config.py
-augment_books add_chapter_summaries --input_dir=json/ --config=configs/add_detailed_chapter_summaries_config.py
+augment_books add_chunk_summaries --input_dir=json/ --config=configs/add_chunk_summaries_detailed_config.py
+augment_books add_chunk_summaries --input_dir=json/ --config=configs/add_chunk_summaries_detailed_reflected_config.py
+augment_books add_chapter_summaries --input_dir=json/ --config=configs/add_chapter_summaries_detailed_config.py
 # 3. Create questions
-augment_books add_chunk_questions --input_dir=json/ --config=configs/add_complex_chunk_questions_config.py
-augment_books add_chapter_questions --input_dir=json/ --config=configs/add_complex_chapter_questions_config.py
+augment_books add_chunk_questions --input_dir=json/ --config=configs/add_chunk_questions_complex_config.py
+augment_books add_chapter_questions --input_dir=json/ --config=configs/add_chapter_questions_complex_config.py
 # 4. Add answers for the questions from step 3
-augment_books add_chunk_answers --input_dir=json/ --config=configs/add_complex_chunk_questions_answers_config.py
-augment_books add_chapter_answers --input_dir=json/ --config=configs/add_complex_chapter_questions_answers_config.py
+augment_books add_chunk_answers --input_dir=json/ --config=configs/add_chunk_questions_answers_complex_config.py
+augment_books add_chapter_answers --input_dir=json/ --config=configs/add_chapter_questions_answers_complex_config.py
 # 5. Create categories
 augment_books add_categories --input_dir=json/ --config=configs/add_categories_config.py
 # 6. Extract Question and answer pairs with metadata
